7dad27b5dcb3aefc59762e13931c35c1f53f57a6
Your Name <fdavi07@gmail.com>
2025-11-17 17:14:44 -0300

    ta la

--- src/cadastro.js
+++ src/cadastro.js
@@ -46,7 +46,7 @@ function validarSenha() {
         alert("Conta criada com sucesso, Bem-Vindo " + usuario);
         document.getElementById("email").value = "";
         document.getElementById("senha").value = "";
-        window.location.href='home.html'
+        window.location.href='projetos.html'
     } else {
         alert(resultadoSenha !== "Conta criada" ? resultadoSenha : resultadoEmail);
     }
